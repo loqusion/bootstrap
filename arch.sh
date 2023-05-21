361537422b9cef133298ee4d8cfa28ceb39fee0e
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-05-21 18:14:41 -0500

    Improve arch automated install

--- arch.sh
+++ arch.sh
@@ -5,17 +5,20 @@ set -euo pipefail
 DEST=$(dirname "$(readlink -f "$0")")
 
 # Install paru
-sudo pacman -S --needed base-devel
-git clone https://aur.archlinux.org/paru.git "$DEST/paru"
-cd "$DEST/paru" && makepkg -si
-
-# Install packages with paru
-paru -S --needed - <"$DEST/pacman.txt"
+if ! command -v paru &>/dev/null; then
+	sudo pacman -S --needed base-devel rustup
+	rustup default stable
+	git clone https://aur.archlinux.org/paru.git "$DEST/paru"
+	cd "$DEST/paru" && makepkg -si && cd "$DEST" && rm -rf "$DEST/paru"
+fi
 
 # Copy etc files
 for file in "$DEST/etc"/*; do
 	sudo cp -riv "$file" /etc
 done
+
+# Install packages with paru
+paru -S --needed - <"$DEST/pacman.txt"
 
 # Enable systemd services
 while read -r service; do
