abacbbfa40d18770d3d8d36291baf0b57a4b6601
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-05-21 18:17:46 -0500

    feat(auth): use ssh for bootstrap

--- auth.sh
+++ auth.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+DIR=$(dirname "$(readlink -f "$0")")
 DEST=${DEST:-"$HOME/.local/share/dotfiles/"}
 
 config() {
@@ -10,3 +11,4 @@ xdg-open https://www.dashlane.com/download
 gh auth login --git-protocol ssh --web
 
 config remote set-url origin git@github.com:loqusion/dotfiles.git
+git -C "$DIR" remote set-url origin git@github.com:loqusion/bootstrap.git
