24de363590b006e65f12360799774121d3539672
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-05-28 18:30:30 -0500

    fix(liveinstall): install openssh

--- liveinstall.sh
+++ liveinstall.sh
@@ -115,7 +115,7 @@ esac
 systemctl restart reflector.service
 
 # shellcheck disable=SC2086
-pacstrap -K /mnt base base-devel alsa-utils "$TARGET_KERNEL" "$KERNEL_HEADERS" linux-firmware intel-ucode iwd dhcpcd man-db man-pages texinfo git github-cli "$SHELL_PACKAGE" "$EDITOR_PACKAGE" "$FS_UTILS_PACKAGE" $ADDITIONAL_PACKAGES
+pacstrap -K /mnt base base-devel alsa-utils "$TARGET_KERNEL" "$KERNEL_HEADERS" linux-firmware intel-ucode iwd dhcpcd openssh man-db man-pages texinfo git github-cli "$SHELL_PACKAGE" "$EDITOR_PACKAGE" "$FS_UTILS_PACKAGE" $ADDITIONAL_PACKAGES
 
 genfstab -U /mnt >>/mnt/etc/fstab
 
