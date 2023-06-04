0f1bd5473d37994c663e5a397bb00e15a3dbf580
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-06-03 23:11:44 -0500

    fix: sparse-checkout multiple files

--- scripts/sparse-checkout.sh
+++ scripts/sparse-checkout.sh
@@ -8,28 +8,33 @@ DOTFILES=${DOTFILES:-"$HOME/.local/share/dotfiles/"}
 FORCE=${FORCE:-false}
 confirmed=false
 
+cd "$DIR"
+
 config() {
 	/usr/bin/git --git-dir="$DOTFILES" --work-tree="$HOME" "$@"
 }
 
-sparse_checkout() {
-	local DOTIGNORE="$1"
-
-	if [ -e "$DOTIGNORE" ]; then
-		files=$(cat "$DOTIGNORE")
-		for file in $files; do
-			[ ! -e "$HOME/$file" ] && continue
-			if [ "$FORCE" != true ] && [ "$confirmed" != true ]; then
-				read -p "WARNING: This is a potentially destructive operation. Continue? [y/N]" -r REPLY
-				[[ $REPLY =~ ^[Yy]$ ]] || exit 1
-				confirmed=true
-			fi
-			rm -rfv "$HOME/.config/${file:?}"
-		done
-		echo "$files" | sed -e 's/^/!.config\//' -e $'1i\\\n/*' -e $'1i\\\n!README.md' | config sparse-checkout set --stdin --no-cone
-	fi
+_cat() {
+	cat "$@" 2>/dev/null || true
 }
 
-PLATFORM=$("$DIR"/scripts/detect-platform.sh)
-sparse_checkout "$DIR/profiles/__common__/$PLATFORM/dotignore.txt"
-sparse_checkout "$DIR/profiles/$HOSTNAME/dotignore.txt"
+sparse_checkout() {
+	local files="$1"
+
+	for file in $files; do
+		{ [ -z "$file" ] || [ ! -e "$HOME/.config/$file" ]; } && continue
+		if [ "$FORCE" != true ] && [ "$confirmed" != true ]; then
+			read -p "WARNING: This is a potentially destructive operation. Continue? [y/N] " -r REPLY
+			[[ "$REPLY" =~ ^[Yy]$ ]] || exit 1
+			confirmed=true
+		fi
+		rm -rfv "$HOME/.config/${file:?}"
+	done
+	echo "$files" | sed -e '/^[[:space:]]*$/d' -e 's/^/!.config\//' -e $'1i\\\n/*' -e $'1i\\\n!README.md' | config sparse-checkout set --stdin --no-cone
+}
+
+PLATFORM=$(./scripts/detect-platform.sh)
+files=$(_cat "$DIR/profiles/__common__/$PLATFORM/dotignore.txt" "$DIR/profiles/$HOSTNAME/dotignore.txt")
+if [ -n "$files" ]; then
+	sparse_checkout "$files"
+fi
