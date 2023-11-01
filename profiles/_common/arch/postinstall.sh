bd38cce1ad356825dd34371bd85db4a6ec02a6c8
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-11-01 01:29:26 -0500

    fix(post): snapper

--- profiles/_common/arch/postinstall.sh
+++ profiles/_common/arch/postinstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Create snapper configs
-if lsblk -f -o FSTYPE | sed -e '/FSTYPE/d' -e '/^$/d' | grep btrfs &>/dev/null; then
+if lsblk -f -o FSTYPE /dev/nvme0n1p3 | sed -e '/FSTYPE/d' -e '/^$/d' | grep btrfs &>/dev/null; then
 	sudo snapper -c root create-config /
 fi
