0423bbf4ad0c89174e83a997ef952324c2cd55ea
John Bernard <38332081+loqusion@users.noreply.github.com>
2023-11-07 08:14:32 -0600

    feat: don't install catppuccin steam

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -7,4 +7,5 @@ exists() {
 exists bat && bat cache --build
 exists tldr && command tldr --update
 [ -e ~/.config/catppuccin/obs/install.sh ] && ~/.config/catppuccin/obs/install.sh
-[ -e ~/.config/catppuccin/steam/install.sh ] && ~/.config/catppuccin/steam/install.sh
+# doesn't work anymore; see https://github.com/catppuccin/steam/issues/26
+# [ -e ~/.config/catppuccin/steam/install.sh ] && ~/.config/catppuccin/steam/install.sh
